d9e3fadd839b98ea58a879c3d82354e2e333fab6
Abraham Nieva <a_nieva@hotmail.com>
2021-04-14 18:35:56 +0100

    modification for using postgre db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_13_150631) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "sharks", force: :cascade do |t|
     t.string "name"
     t.text "facts"
